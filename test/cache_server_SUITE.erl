84398dd19167cc5c797e236cda8280db7138ceac
bukv <bumusic13@gmail.com>
2020-12-20 22:50:56 +0200

    new tests for: insert_with_db, db_lookup, db_lookup_by_date, db_delete_item

--- test/cache_server_SUITE.erl
+++ test/cache_server_SUITE.erl
@@ -15,9 +15,13 @@
 -export([
     start_link/1,
     insert/1,
+    insert_from_list/1,
     insert_with_db/1,
     lookup/1,
+    db_lookup/1,
     lookup_by_date/1,
+    db_lookup_by_date/1,
+    db_delete_item/1,
     stop/1
     ]).
 
@@ -25,8 +29,13 @@
 all() -> [
     start_link, 
     insert,
+    insert_from_list,
+    insert_with_db,
     lookup,
+    db_lookup,
     lookup_by_date,
+    db_lookup_by_date,
+    db_delete_item,
     stop
     ].
 
@@ -37,12 +46,10 @@ init_per_suite(Config) ->
     Key1 = {key1, key_1},
     Val1 = {val1, val_1},
     TTL1 = {ttl1, 60},
-    [DropInterval, TableName, Key1, Val1, TTL1|Config].
-
-init_per_testcase(lookup_by_date, Config) ->
     DateFrom = {date_from,{{2015,1,1},{00,00,00}}},
-    DateTo = {date_to,{{2015,1,10},{23,59,59}}},
-    [DateFrom,DateTo|Config];
+    DateTo = {date_to,{{2025,1,10},{23,59,59}}},
+    [DateFrom, DateTo, DropInterval, TableName, Key1, Val1, TTL1|Config].
+
 init_per_testcase(_, Config) ->
     Config.
 
@@ -68,6 +75,15 @@ insert(Config) ->
     cache_server:start_link(TableName, [{drop_interval,DropInterval}]),
     ok = cache_server:insert(TableName, Key1, Val1, TTL1).
 
+insert_from_list(Config) ->
+    TableName = proplists:get_value(table_name, Config),
+    DropInterval = proplists:get_value(drop_interval, Config),
+    Key1 = proplists:get_value(key1, Config),
+    Val1 = proplists:get_value(val1, Config),
+    TTL1 = proplists:get_value(ttl1, Config),
+    cache_server:start_link(TableName, [{drop_interval,DropInterval}]),
+    ok = cache_server:insert_from_list(TableName, [{Key1, Val1}], TTL1).
+
 insert_with_db(Config) ->
     TableName = proplists:get_value(table_name, Config),
     DropInterval = proplists:get_value(drop_interval, Config),
@@ -87,6 +103,14 @@ lookup(Config) ->
     cache_server:insert(TableName, Key1, Val1, TTL1), 
     {ok,Val1} = cache_server:lookup(TableName, Key1).
 
+db_lookup(Config) ->
+    TableName = proplists:get_value(table_name, Config),
+    DropInterval = proplists:get_value(drop_interval, Config),
+    Key1 = proplists:get_value(key1, Config),
+    Val1 = proplists:get_value(val1, Config),
+    cache_server:start_link(TableName, [{drop_interval,DropInterval}]),
+    {ok,Val1} = cache_server:db_lookup(TableName, Key1).
+
 lookup_by_date(Config) ->
     TableName = proplists:get_value(table_name, Config),
     DropInterval = proplists:get_value(drop_interval, Config),
@@ -94,6 +118,23 @@ lookup_by_date(Config) ->
     DateTo = proplists:get_value(date_to, Config),
     cache_server:start_link(TableName, [{drop_interval,DropInterval}]),
     [] = cache_server:lookup_by_date(TableName, DateFrom, DateTo).
+
+db_lookup_by_date(Config) ->
+    TableName = proplists:get_value(table_name, Config),
+    DropInterval = proplists:get_value(drop_interval, Config),
+    Key1 = proplists:get_value(key1, Config),
+    Val1 = proplists:get_value(val1, Config),
+    DateFrom = proplists:get_value(date_from, Config),
+    DateTo = proplists:get_value(date_to, Config),
+    cache_server:start_link(TableName, [{drop_interval,DropInterval}]),
+    [{Key1,Val1}] = cache_server:db_lookup_by_date(DateFrom, DateTo).
+
+db_delete_item(Config) ->
+    TableName = proplists:get_value(table_name, Config),
+    DropInterval = proplists:get_value(drop_interval, Config),
+    Key1 = proplists:get_value(key1, Config),
+    cache_server:start_link(TableName, [{drop_interval,DropInterval}]),
+    ok = cache_server:db_delete_item(Key1).
 
 stop(Config) ->
     TableName = proplists:get_value(table_name, Config),
